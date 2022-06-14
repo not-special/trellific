f50dc95592e9c9a25999796492ea0858d68df912
Tim Dronkers <tdronkers@gmail.com>
2022-06-14 12:31:02 -0600

    refactored board, list models; added populate method to route get /boards/:id

--- server/models/list.js
+++ server/models/list.js
@@ -7,19 +7,18 @@ const ListSchema = new Schema(
       type: String,
       required: [true, 'The List title is required']
     },
-    boardId: {
-      type: ObjectId,
+    board: {
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Board"
     },
     position: {
       type: Number,
       required: true,
       default: 65535
-    }
+    },
   }
   , { timestamps: true }
 );
 
 const List = mongoose.model('List', ListSchema);
-
 module.exports = List;
