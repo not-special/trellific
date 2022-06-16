6588474e04d3ef137ee6e5d8cfdcbc15e92bad7d
Scott Graham <scttgrhm7+public@gmail.com>
2022-06-16 08:11:48 -0700

    fix html classNames
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- client/src/components/board/Header.jsx
+++ client/src/components/board/Header.jsx
@@ -5,14 +5,14 @@ const Header = ({ title }) => {
     <header>
       <ul>
         <li id="title">{title}</li>
-        <li class="star-icon icon"></li>
-        <li class="private private-icon icon">Private</li>
-      </ul><div class="menu">
-      <i class="more-icon sm-icon"></i>
+        <li className="star-icon icon"></li>
+        <li className="private private-icon icon">Private</li>
+      </ul><div className="menu">
+      <i className="more-icon sm-icon"></i>
         Show Menu
       </div>
-      <div class="subscribed">
-        <i class="sub-icon sm-icon"></i>
+      <div className="subscribed">
+        <i className="sub-icon sm-icon"></i>
         Subscribed
       </div>
     </header>
