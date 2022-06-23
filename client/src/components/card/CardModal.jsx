f000f2203e2ebce08b6b14f6292ca111bc0f3bcd
Tim Dronkers <tdronkers@gmail.com>
2022-06-23 12:21:09 -0600

    dynamic rendering of labels in card modal view
    Co-authored-by Scott Graham <scttgrhm7+public@gmail.com>

--- client/src/components/card/CardModal.jsx
+++ client/src/components/card/CardModal.jsx
@@ -106,6 +106,26 @@ const CardModal = () => {
     e.preventDefault();
   }
 
+  const renderLabels = () => {
+    if (activeCard) {
+      return (
+        <li className="labels-section">
+          <h3>Labels</h3>    
+          {activeCard.labels.map(color => {
+            return (
+                <div key={color} className="member-container">
+                  <div className={`${color} label colorblindable`}></div>
+                </div>
+            )
+          })}
+          <div className="member-container" onClick={toggleLabelsPopover}>
+            <i className="plus-icon sm-icon"></i>
+          </div>
+       </li> 
+      )
+    }
+  }
+
   const dueDate = () => {
     if (activeCard && activeCard.dueDate) {
       return (
@@ -177,30 +197,7 @@ const CardModal = () => {
           <ul className="modal-outer-list">
             <li className="details-section">
               <ul className="modal-details-list">
-                <li className="labels-section">
-                  <h3>Labels</h3>
-                  <div className="member-container">
-                    <div className="green label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <div className="yellow label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <div className="orange label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <div className="blue label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <div className="purple label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <div className="red label colorblindable"></div>
-                  </div>
-                  <div className="member-container">
-                    <i className="plus-icon sm-icon"></i>
-                  </div>
-                </li>
+                { renderLabels() }
                 { dueDate() }
               </ul>
               <form className="description">
