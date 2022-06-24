9fca3c0c7f7aa14185a6ac9ac465d94d01eb6412
Scott Graham <scttgrhm7+public@gmail.com>
2022-06-24 10:33:06 -0700

    add labels to board view cards
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- client/src/components/board/Card.jsx
+++ client/src/components/board/Card.jsx
@@ -9,6 +9,7 @@ const Card = ({ card }) => {
             <i className="edit-toggle edit-icon sm-icon"></i>
             <div className="cover-image"></div>
             <div className="card-info">
+              {card.labels.map(color => <div key={color} className={`card-label ${color} colorblindable`}></div>)}
               <p>{card.title}</p>
             </div>
             <div className="card-icons"></div>
